19ac774f24e80f94398cedf6425f4131c00d407a
Pavel P <markee109582@yandex.ru>
2023-05-27 01:28:32 +0300

    Add random with Stack

--- StackTest/StackTest.java
+++ StackTest/StackTest.java
@@ -1,7 +1,29 @@
+import java.util.EmptyStackException;
 import java.util.Stack;
+import java.util.Random;
 public class StackTest {
     public static void main(String[] args) {
+        char[] InputArray = {'a', 'p', 'm', 'f', 'r'};
         Stack Food = new Stack();
+        Random random = new Random();
+        int randomnumber;
+
+        for (int i = 0; i<InputArray.length; i++){
+            Food.add(InputArray[i]);
+        }
+        System.out.println(InputArray);
+        System.out.println(Food);
+
+        for (int i = 0; i < InputArray.length; i++){
+            try {
+                randomnumber = random.nextInt(Food.size());
+                InputArray[i] = (char) Food.get(randomnumber);
+                Food.remove(randomnumber);
+            } catch (EmptyStackException e){
+                System.out.println("Stack Empty");
+            }
+        }
+        System.out.println(InputArray);
         System.out.println(Food);
     }
 }
