86fe7f08f9bc6e63796642d4c796b5dc20bf1268
Pavel P <markee109582@yandex.ru>
2023-05-27 22:39:51 +0300

    Fix random

--- Food/Food.java
+++ Food/Food.java
@@ -2,38 +2,40 @@ import java.util.Random;
 public class Food {
     public static void main(String[] args) {
         //Test how work random
-        Random rand = new Random();
-        int r = 0, i = 0;
+        Random random = new Random();
+        int randomizer = 0, counter = 0;
 
-        while (r != 5){
-            r = rand.nextInt(10);
-            System.out.print(r);
-            if (r == 0) i++;
+        while (randomizer != 5){
+            randomizer = random.nextInt(10);
+            System.out.print(randomizer);
+            if (randomizer == 0) counter++;
         }
         System.out.println();
-        System.out.println("Found zero " + i + " times");
+        System.out.println("Found zero " + counter + " times");
 
-        //Random all food + without repeats
+        //Random elements + create array elements without repeats
         char[] InputArray = {'a', 'p', 'm', 'f', 'r'};
         char[] OutputArray = new char[InputArray.length];
-        int k = 0, rnd, counter = 0;
+        int foundrepeat = 0;
 
-        rnd = rand.nextInt(InputArray.length);
-        OutputArray[k] = InputArray[rnd];
-        k++;
+        //Add 1st element
+        counter = 0;
+        randomizer = random.nextInt(InputArray.length);
+        OutputArray[counter] = InputArray[randomizer];
+        counter++;
 
-        while (k != InputArray.length) {
-            rnd = rand.nextInt(InputArray.length);
-            for (int j = 0; j < k; j++) {
-            if (OutputArray[j] == InputArray[rnd]) {
-                counter = 0;
+        while (counter != InputArray.length) {
+            randomizer = random.nextInt(InputArray.length);             //random
+            for (int j = 0; j < counter; j++) {
+            if (OutputArray[j] == InputArray[randomizer]) {             //if we search repeat --> random again
+                foundrepeat = 0;
                 break;
-            } else {
-                counter++;
-                if (counter == k) {
-                    OutputArray[k] = InputArray[rnd];
-                    k++;
-                    counter = 0;
+            } else {                                                    //add element in array if 0 repeats
+                foundrepeat++;
+                if (foundrepeat == counter) {
+                    OutputArray[counter] = InputArray[randomizer];
+                    counter++;
+                    foundrepeat = 0;
                     break;
                     }
                 }
