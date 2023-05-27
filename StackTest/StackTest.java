5e680a8b7b364bd7795668e3a671213436bf00a7
Pavel P <markee109582@yandex.ru>
2023-05-27 22:01:31 +0300

    Add notes

--- StackTest/StackTest.java
+++ StackTest/StackTest.java
@@ -7,10 +7,13 @@ public class StackTest {
         Stack Food = new Stack();
         Random random = new Random();
 
+        //Add elements in Stack
         for (char element : InputArray) Food.add(element);
         System.out.println(InputArray);
         System.out.println(Food);
 
+        //Random element from Stack + rewrite array + clear Stack
+        //try catch can be deleted
         for (int i = 0; i < InputArray.length; i++){
             try {
                 int randomizer = random.nextInt(Food.size());
