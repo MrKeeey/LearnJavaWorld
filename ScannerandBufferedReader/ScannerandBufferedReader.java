04ceddcc59e53550570e4c0633b49f01237ad902
Pavel P <markee109582@yandex.ru>
2023-06-08 23:58:24 +0300

    add //

--- ScannerandBufferedReader/ScannerandBufferedReader.java
+++ ScannerandBufferedReader/ScannerandBufferedReader.java
@@ -6,13 +6,16 @@ public class ScannerandBufferedReader {
     public static void main(String[] args) throws IOException {
         String[] TestString = new String[3];
 
+        //input with scanner
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < 3; i++) TestString[i] = scanner.nextLine();
         System.out.println(Arrays.toString(TestString));
 
+        //input with reader
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < 3; i++) TestString[i] = reader.readLine();
         System.out.println(Arrays.toString(TestString));
+        //System.out.println(scanner);
         //int chislo = Integer.parseInt(TestString[0]);
     }
 }
