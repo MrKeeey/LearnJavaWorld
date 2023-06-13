54ec659c147f0e54ee74fef591dd6a80926830a2
Pavel P <markee109582@yandex.ru>
2023-06-13 23:25:22 +0300

    little fix + branch

---
+++ LearnWithBook/chapter3/DogCat.java
@@ -0,0 +1,22 @@
+package LearnWithBook.chapter3;
+
+class Dog{
+    void bark(){
+        System.out.println("Woof Woof");
+    }
+}
+
+class Cat{
+    void meow(){
+        System.out.println("Meooooow");
+    }
+}
+public class DogCat {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        Cat d = new Cat();
+
+        //d.bark();
+        d.meow();
+    }
+}
