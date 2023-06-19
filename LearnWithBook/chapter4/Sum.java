58b87fc9ec481c9fa6f1d2ec4dbe9d1f033f1f2a
Pavel P <markee109582@yandex.ru>
2023-06-19 23:09:38 +0300

    add Sum

---
+++ LearnWithBook/chapter4/Sum.java
@@ -0,0 +1,25 @@
+package LearnWithBook.chapter4;
+class SumTest {
+    public static void main(String[] args) {
+
+        int firstnumber = 234, secondnumber = -92;
+        Sum summa = new Sum();
+
+        summa.go();
+        summa.SummResult(-2343, 2863);
+        summa.SummResult(firstnumber, secondnumber);
+        System.out.println();
+    }
+}
+public class Sum {
+
+    void go (){
+        SummResult(12, 24);
+    }
+
+    void SummResult (int firstnumber, int secondnumber){
+        int result = firstnumber + secondnumber;
+        System.out.println("Summ your numbers is " + result);
+    }
+}
+
