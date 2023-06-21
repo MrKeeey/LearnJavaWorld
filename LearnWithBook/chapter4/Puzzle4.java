206a5025a1fecde0b820dd77f3670380eddcdf0e
Pavel P <markee109582@yandex.ru>
2023-06-21 22:05:22 +0300

    add Puzzle4

---
+++ LearnWithBook/chapter4/Puzzle4.java
@@ -0,0 +1,36 @@
+package LearnWithBook.chapter4;
+public class Puzzle4 {
+    public static void main(String[] args) {
+
+        int y = 1;
+        int x = 0;
+        int result = 0;
+        Puzzle4b[] obs = new Puzzle4b[6];
+
+        while(x < 6) {
+            obs[x] = new Puzzle4b();
+            obs[x].ivar = y;
+            y = y * 10;
+            x++;
+        }
+
+        x = 6;
+        while (x > 0) {
+            x--;
+            result = result + obs[x].doStuff(x);
+        }
+        System.out.println("Result: " + result);
+    }
+}
+
+class Puzzle4b {
+    int ivar;
+    public int doStuff(int factor) {
+
+        if (ivar > 100) {
+            return ivar * factor;
+        } else {
+            return ivar * (5 - factor);
+        }
+    }
+}
