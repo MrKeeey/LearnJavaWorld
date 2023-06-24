925454e3d286162e98ebf560331f4cb4d2a5e5b7
Pavel P <markee109582@yandex.ru>
2023-06-24 23:13:21 +0300

    fix game

--- LearnWithBook/chapter5/SimpleDotComGame.java
+++ LearnWithBook/chapter5/SimpleDotComGame.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 public class SimpleDotComGame {
     public static void main(String[] args) {
         SimpleDotCom play = new SimpleDotCom();
-        //Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         //System.out.println(play.checkYourself(scanner.nextLine()));
         int[] locations = {2,3,4};
         play.setLocationCells(locations);
         String userGuest = "2";
         while (play.numOfHits != 3){
-            String result = play.checkYourself(userGuest);
+            String result = (String) play.checkYourself(scanner.nextLine());
+            //String result = play.checkYourself(userGuest);
             System.out.println(result);
         }
 
