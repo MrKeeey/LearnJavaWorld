094af79a6e0e697334aeecb3b8fc39bb486a31ba
Pavel P <markee109582@yandex.ru>
2023-06-25 16:23:53 +0300

    fix game

--- LearnWithBook/chapter5/SimpleDotComGame.java
+++ LearnWithBook/chapter5/SimpleDotComGame.java
@@ -5,18 +5,23 @@ public class SimpleDotComGame {
         SimpleDotCom play = new SimpleDotCom();
         Scanner scanner = new Scanner(System.in);
         int[] locations = {2,3,4};
+        int count = 0;
         //String userGuest = "2";
 
         play.setLocationCells(locations);
         while (play.numOfHits != locations.length){
+            System.out.print("Enter a number: ");
             try {
                 play.checkYourself(scanner.nextLine());
             } catch(NumberFormatException e) {
                 System.out.println("Wrong symbol. You can enter only numbers. Try again...");
             }
+            count++;
             //String result = play.checkYourself(userGuest);
         }
-
+        if (count == locations.length) {
+            System.out.print("You took " + count + " guesses. Great, this is the minimum number of attempts.");
+        } else System.out.print("You took " + count + " guesses.");
     }
 }
 
@@ -35,12 +40,14 @@ class SimpleDotCom {
                 break;
             }
         }
+
         if (numOfHits == locationCells.length) result = "Kill";
+
         System.out.println(result);
         return result;
     }
 
-    void setLocationCells (int[] loc){
-        locationCells = loc;
+    void setLocationCells (int[] locs){
+        locationCells = locs;
     }
 }
