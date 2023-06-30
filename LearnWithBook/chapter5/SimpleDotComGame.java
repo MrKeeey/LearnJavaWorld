fe3d9cc843b3b49893ed85937e50ad80ccdc3898
Pavel P <markee109582@yandex.ru>
2023-06-30 20:43:21 +0300

    fix chapter 4 SimpleDotComGame

--- LearnWithBook/chapter5/SimpleDotComGame.java
+++ LearnWithBook/chapter5/SimpleDotComGame.java
@@ -7,6 +7,8 @@ public class SimpleDotComGame {
         int randomNum = (int) (Math.random() * 5);
         int[] locations = {randomNum, randomNum + 1, randomNum + 2};
         int count = 0;
+        int[] hitarr = {0, 1, 2, 3, 4, 5, 6};
+        boolean[] flag = new boolean[7];
         String userGuess;
 
         play.setLocationCells(locations);
@@ -14,14 +16,18 @@ public class SimpleDotComGame {
             System.out.print("Enter a number: ");
             try {
                 userGuess = scanner.nextLine();
-                if (Integer.parseInt(userGuess) == play.lastGuess) {
-                    System.out.println("You already use this number. Try again.");
-                } else {
-                    play.checkYourself(userGuess);
-                    count++;
-                }
+                int intuserGuess = Integer.parseInt(userGuess);
+                    if (intuserGuess == hitarr[intuserGuess] && flag[intuserGuess]) {
+                        System.out.println("You already use this number. Try again.");
+                    } else {
+                        play.checkYourself(userGuess);
+                        flag[intuserGuess] = true;
+                        count++;
+                    }
             } catch(NumberFormatException e) {
                 System.out.println("Wrong symbol. You can enter only numbers. Try again...");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("You can use only 0 - 6 numbers. Try again...");
             }
         }
         if (count == locations.length) {
@@ -32,12 +38,11 @@ public class SimpleDotComGame {
 
 class SimpleDotCom {
     int[] locationCells;
-    int numOfHits = 0, lastGuess = -1;
+    int numOfHits = 0;
     String result = " ";
     String checkYourself (String stringGuess) {
         int guess = Integer.parseInt(stringGuess);
         result = "Miss";
-        lastGuess = guess;
         for (int count = 0; count < locationCells.length; count++) {
             if (guess == locationCells[count]){
                 result = "Hit";
