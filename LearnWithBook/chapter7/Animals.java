a7606cfc9e0ab89d036431d3a4e0cacbd3771e3e
Pavel P <markee109582@yandex.ru>
2023-07-09 21:57:58 +0300

    add animals

--- LearnWithBook/chapter7/Animals.java
+++ LearnWithBook/chapter7/Animals.java
@@ -5,11 +5,11 @@ public class Animals {
 
         Animals[] animals = new Animals[5];
 
-        animals [0] = new Dog();
-        animals [1] = new Cat();
-        animals [2] = new Wolf();
-        animals [3] = new Hippo();
-        animals [4] = new Lion();
+        animals[0] = new Dog();
+        animals[1] = new Cat();
+        animals[2] = new Wolf();
+        animals[3] = new Hippo();
+        animals[4] = new Lion();
 
         for (int count = 0; count < animals.length; count++){
             animals[count].eat();
@@ -17,11 +17,32 @@ public class Animals {
         }
     }
 
-    public eat(){
-
+    public void eat(){
+        System.out.print("Eating");
     }
 
-    public roam(){
+    public void roam(){
 
     }
+}
+
+class Dog extends Animals {
+    @Override
+    public void eat() {
+        super.eat();
+        System.out.println(" Dog ");
+
+    }
+}
+class Cat extends Animals {
+
+}
+class Wolf extends Animals {
+
+}
+class Hippo extends Animals {
+
+}
+class Lion extends Animals {
+
 }
