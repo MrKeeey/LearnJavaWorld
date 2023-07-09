0b5190e0d6c3fe7347a44d7de0ef10b2259b5f12
Pavel P <markee109582@yandex.ru>
2023-07-09 21:40:04 +0300

    add animals

--- LearnWithBook/chapter7/Animals.java
+++ LearnWithBook/chapter7/Animals.java
@@ -2,12 +2,26 @@ package LearnWithBook.chapter7;
 
 public class Animals {
     public static void main(String[] args) {
+
         Animals[] animals = new Animals[5];
+
         animals [0] = new Dog();
-        animals [0] = new Dog();
-        animals [0] = new Dog();
-        animals [0] = new Dog();
-        animals [0] = new Dog();
+        animals [1] = new Cat();
+        animals [2] = new Wolf();
+        animals [3] = new Hippo();
+        animals [4] = new Lion();
+
+        for (int count = 0; count < animals.length; count++){
+            animals[count].eat();
+            animals[count].roam();
+        }
+    }
+
+    public eat(){
+
+    }
+
+    public roam(){
 
     }
 }
