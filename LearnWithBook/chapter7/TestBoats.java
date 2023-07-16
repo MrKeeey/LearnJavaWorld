e79d9c3cbcab9b4a056402ce281a555c86e33739
Pavel P <markee109582@yandex.ru>
2023-07-16 23:01:40 +0300

    fix TestBoats

--- LearnWithBook/chapter7/TestBoats.java
+++ LearnWithBook/chapter7/TestBoats.java
@@ -2,10 +2,21 @@ package LearnWithBook.chapter7;
 
 public class TestBoats {
     public static void main(String[] args) {
+        Boat b1 = new Boat();
+        Sailboat b2 = new Sailboat();
+        Rowboat b3 = new Rowboat();
+
 
     }
 }
 
-class Rowboat {
+class Boat {
+
+}
+class Rowboat extends Boat {
+
+}
+
+class Sailboat extends Boat {
 
 }
