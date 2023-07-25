a4fdfa6aed0000f22b93874e01b5a80999d198bc
Pavel P <markee109582@yandex.ru>
2023-07-25 23:19:43 +0300

    add Animals

---
+++ LearnWithBook/chapter8/Animals.java
@@ -0,0 +1,38 @@
+public class Animals {
+    public static void main(String[] args) {
+
+    }
+}
+ abstract class Canine extends Animals {
+
+ }
+
+ abstract class Feline extends Animals {
+
+ }
+
+class Hippo {
+
+}
+
+class Dog extends Canine {
+
+}
+
+class Wolf extends Canine {
+
+}
+
+class Cat extends Feline {
+
+}
+
+class Lion extends Feline {
+
+}
+
+class Tiger extends Feline {
+
+}
+
+
