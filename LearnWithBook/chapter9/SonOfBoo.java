4a2931d220070a63675dba463d10722d5c275cec
Pavel P <markee109582@yandex.ru>
2023-08-05 22:34:19 +0300

    fix SonOfBoo

--- LearnWithBook/chapter9/SonOfBoo.java
+++ LearnWithBook/chapter9/SonOfBoo.java
@@ -12,23 +12,23 @@ public class SonOfBoo extends Boo {
     public SonOfBoo(String s){
         super(42);
     }
-
-    public SonOfBoo(int i, String s){
+/*
+    public SonOfBoo(int i, String s){                   //3 pic
 
     }
 
-    public SonOfBoo(String a, String b, String c){
+    public SonOfBoo(String a, String b, String c){      //1 pic
         super(a, b);
     }
-
+*/
     public SonOfBoo(int i, int j){
         super("man", j);
     }
-
-    public SonOfBoo(int i, int x, int y){
+/*
+    public SonOfBoo(int i, int x, int y){               //2 pic
         super(i, "super");
     }
-
+*/
     public static void main(String[] args) {
         System.out.println("Working!");
     }
