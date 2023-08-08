cdce08a1ab5ed23c4bc8b384049bfb197b2e657f
Pavel P <markee109582@yandex.ru>
2023-08-08 23:36:18 +0300

    add TestLifeSupportSim

---
+++ LearnWithBook/chapter9/TestLifeSupportSim.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class V2Radiator {
+    V2Radiator(ArrayList list){
+        for (int count = 0; count < 5; count++){
+            list.add(new SimUnit("V2Radiator"));
+        }
+    }
+}
+
+class V3Radiator extends V2Radiator {
+    V3Radiator(ArrayList lglist){
+        super(lglist);
+        for (int count = 0; count < 10; count++){
+            lglist.add(new SimUnit("V3Radiator"));
+        }
+    }
+}
+
+class RetentionBot {
+    RetentionBot(ArrayList rlist){
+        rlist.add(new SimUnit("Hot Machine"));
+    }
+}
+
+public class TestLifeSupportSim {
+    public static void main(String[] args) {
+        ArrayList aList = new ArrayList();
+        V2Radiator v2 = new V2Radiator(aList);
+        V3Radiator v3 = new V3Radiator(aList);
+        for (int count = 0; count < 20; count++){
+            RetentionBot ret = new RetentionBot(aList);
+        }
+    }
+}
+
+class SimUnit {
+    String botType;
+    SimUnit(String type){
+        botType = type;
+    }
+
+    int powerUse(){
+        if ("Hot Machine".equals(botType)){
+            return 2;
+        } else {
+            return 4;
+        }
+    }
+}
