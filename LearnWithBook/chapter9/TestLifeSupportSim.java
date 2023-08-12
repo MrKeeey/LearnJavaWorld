37295b8b1b191db3336bffac92aa2d91189261ab
Pavel P <markee109582@yandex.ru>
2023-08-12 10:20:34 +0300

    fix TestLifeSupportSim

--- LearnWithBook/chapter9/TestLifeSupportSim.java
+++ LearnWithBook/chapter9/TestLifeSupportSim.java
@@ -1,32 +1,22 @@
 import java.util.*;
 
 class V2Radiator {
-    public String infov2 = "V2Radiator";
-
     V2Radiator(ArrayList list){
         for (int count = 0; count < 5; count++){
-            this.infov2 = infov2;
-            list.add(new SimUnit(infov2));
+            list.add(new SimUnit("V2Radiator"));
         }
-    }
-    @Override
-    public String toString(){
-        return infov2 + " ";
     }
 }
 
-class V3Radiator extends V2Radiator {
-    public String infov3 = "V3Radiator";
+class V3Radiator {
     V3Radiator(ArrayList lglist){
-        super(lglist);
         for (int count = 0; count < 10; count++){
-            lglist.add(new SimUnit(infov3));
+            lglist.add(new SimUnit("V3Radiator"));
         }
     }
 }
 
 class RetentionBot {
-
     RetentionBot(ArrayList rlist){
         rlist.add(new SimUnit("Hot Machine"));
     }
@@ -43,12 +33,7 @@ public class TestLifeSupportSim {
         for (int count = 0; count < 20; count++){
             RetentionBot ret = new RetentionBot(aList);
         }
-        for (Object al : aList) {
-            System.out.print(al + " ");
-        }
-    }
-    public String toString(){
-        return "123";
+        System.out.println(aList);
     }
 }
 
@@ -56,15 +41,17 @@ class SimUnit {
     String botType;
     SimUnit(String type){
         botType = type;
+        powerUse();
     }
 
     int powerUse(){
         if ("Hot Machine".equals(botType)){
-            System.out.println("2");
             return 2;
         } else {
-            System.out.println("4");
             return 4;
         }
+    }
+    public String toString(){
+        return botType;
     }
 }
