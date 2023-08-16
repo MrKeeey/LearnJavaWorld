47ee653aef7057969dcce2fd8628ff19622cbd1f
Pavel P <markee109582@yandex.ru>
2023-08-16 09:23:45 +0300

    test Math

--- LearnWithBook/chapter10/Test.java
+++ LearnWithBook/chapter10/Test.java
@@ -2,6 +2,9 @@ package LearnWithBook.chapter10;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Happy B day!");
+        int rnd = (int) Math.round(42.2);
+        int min = Math.min(56, 12);
+        int abs = Math.abs(-343);
+        System.out.println(rnd + " " + min + " " + abs);
     }
 }
