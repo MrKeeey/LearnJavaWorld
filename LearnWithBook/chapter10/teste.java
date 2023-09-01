49e997b002d5d426c9bf93bd6d191c0a8c76d122
Pavel P <markee109582@yandex.ru>
2023-09-01 23:08:01 +0300

    fix tt

---
+++ LearnWithBook/chapter10/teste.java
@@ -0,0 +1,13 @@
+package LearnWithBook.chapter10;
+
+public class teste {
+    public static void main(String[] args) {
+        tt TT = new tt();
+    }
+}
+
+class tt {
+    tt(){
+       System.out.println("TT");
+    }
+}
