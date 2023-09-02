ad6c865c96fd7c6602dca11372ab6ed007149b7d
Pavel P <markee109582@yandex.ru>
2023-09-02 23:30:42 +0300

    fix tt

--- LearnWithBook/chapter10/teste.java
+++ LearnWithBook/chapter10/teste.java
@@ -3,11 +3,16 @@ package LearnWithBook.chapter10;
 public class teste {
     public static void main(String[] args) {
         tt TT = new tt();
+        System.out.println(TT.name);
+        System.out.println(tt.names);
     }
 }
 
 class tt {
+    final String name = "Meow";
+    static String names = "So much meow";
     tt(){
        System.out.println("TT");
     }
+
 }
