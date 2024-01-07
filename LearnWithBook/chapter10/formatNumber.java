8331050378a68820766e348fac08d78452c9d609
Pavel P <markee109582@yandex.ru>
2024-01-07 18:05:58 +0300

    fix formatNumber

--- LearnWithBook/chapter10/formatNumber.java
+++ LearnWithBook/chapter10/formatNumber.java
@@ -7,5 +7,12 @@ public class formatNumber {
 
         str = String.format("I want to change %,.2f errors", 1233444.4987);
         System.out.println(str);
+
+        System.out.println(String.format("%d", 42));
+        System.out.println(String.format("%.3f", 42.000000));
+        System.out.println(String.format("%x", 42));
+        System.out.println(String.format("%c", 42));
+
+        System.out.println(String.format("I have %d marks form %d marks", 88, 100));
     }
 }
