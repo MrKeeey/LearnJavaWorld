120c61776beaf0f3e7bd281776ef848b1c8b4cd3
Pavel P <markee109582@yandex.ru>
2024-01-11 11:57:39 +0300

    add MusicTest1

--- LearnWithBook/chapter11/MusicTest1.java
+++ LearnWithBook/chapter11/MusicTest1.java
@@ -1,7 +1,19 @@
 package LearnWithBook.chapter11;
 
-public class MusicTest1 {
-    public static void main(String[] args) {
+import javax.sound.midi.*;
 
+public class MusicTest1 {
+    public void play() {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            System.out.println("Got it!");
+        } catch(MidiUnavailableException ex) {
+            System.out.println("Fail");
+            ex.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        MusicTest1 mt = new MusicTest1();
+        mt.play();
     }
 }
