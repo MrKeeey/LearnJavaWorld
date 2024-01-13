0e71f616a51e7eb2d94023122c4d2f9d9af6fa97
Pavel P <markee109582@yandex.ru>
2024-01-13 17:17:29 +0300

    add MiniMiniMusicApp

---
+++ LearnWithBook/chapter11/MiniMiniMusicApp.java
@@ -0,0 +1,16 @@
+package LearnWithBook.chapter11;
+
+public class MiniMiniMusicApp {
+    public static void main(String[] args) {
+        MiniMiniMusicApp mini = new MiniMiniMusicApp();
+        mini.play();
+    }
+
+    public void play(){
+        try {
+
+        } catch (Exception ex) {
+
+        }
+    }
+}
