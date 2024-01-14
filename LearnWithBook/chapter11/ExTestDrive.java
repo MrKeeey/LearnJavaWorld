960baf3811e98be235c81e54aa705676cdf29935
Pavel P <markee109582@yandex.ru>
2024-01-14 19:30:10 +0300

    fix ExTestDrive

--- LearnWithBook/chapter11/ExTestDrive.java
+++ LearnWithBook/chapter11/ExTestDrive.java
@@ -4,18 +4,17 @@ class MyEx extends Exception { }
 
 public class ExTestDrive {
     public static void main(String[] args) {
-        String test = "no";
-        System.out.print("t");
+        String test = args[0];
         try {
+            System.out.print("t");
             doRisky(test);
-            System.out.print("r");
             System.out.print("o");
         } catch (MyEx e) {
             System.out.print("a");
         } finally {
             System.out.print("w");
-            System.out.println("s");
         }
+        System.out.println("s");
     }
 
     static void doRisky(String t) throws MyEx {
@@ -23,5 +22,6 @@ public class ExTestDrive {
         if ("yes".equals(t)) {
             throw new MyEx();
         }
+        System.out.print("r");
     }
 }
