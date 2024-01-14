4bce80e51df3cf2249236f5bf85765b13066c19c
Pavel P <markee109582@yandex.ru>
2024-01-14 21:08:42 +0300

    fix dich

--- LearnWithBook/chapter11/ExTestDrive.java
+++ LearnWithBook/chapter11/ExTestDrive.java
@@ -4,6 +4,7 @@ class MyEx extends Exception { }
 
 public class ExTestDrive {
     public static void main(String[] args) {
+
         String test = args[0];
         try {
             System.out.print("t");
---
+++ LearnWithBook/chapter11/test.java
@@ -0,0 +1,9 @@
+package LearnWithBook.chapter11;
+
+public class test {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Fail");
+        } else System.out.println(args[0]);
+    }
+}
