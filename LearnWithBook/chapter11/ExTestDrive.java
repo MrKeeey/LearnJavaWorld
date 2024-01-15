724f98bdb556b4611875b79adf01ce17e0509ba6
Pavel P <markee109582@yandex.ru>
2024-01-15 14:02:30 +0300

    fix chapter11

--- LearnWithBook/chapter11/ExTestDrive.java
+++ LearnWithBook/chapter11/ExTestDrive.java
@@ -20,9 +20,7 @@ public class ExTestDrive {
 
     static void doRisky(String t) throws MyEx {
         System.out.print("h");
-        if ("yes".equals(t)) {
-            throw new MyEx();
-        }
+        if ("yes".equals(t)) throw new MyEx();
         System.out.print("r");
     }
 }
--- LearnWithBook/chapter11/test.java
+++
@@ -1,9 +0,0 @@
-package LearnWithBook.chapter11;
-
-public class test {
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Fail");
-        } else System.out.println(args[0]);
-    }
-}
