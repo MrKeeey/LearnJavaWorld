679657a8038c7b4c25aa1bc3fcca858d9bbdfaa3
Pavel P <markee109582@yandex.ru>
2024-01-17 18:27:52 +0300

    fix SimpleGui1

--- LearnWithBook/chapter12/SimpleGui1.java
+++ LearnWithBook/chapter12/SimpleGui1.java
@@ -5,24 +5,34 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SimpleGui1 implements ActionListener {
-    JButton button;
+    JButton button, button2;
     public static void main(String[] args) {
         SimpleGui1 gui = new SimpleGui1();
         gui.go();
+
     }
 
     public void go() {
-        JFrame frame = new JFrame();
-        button = new JButton("Interview");
-        button.addActionListener(this);
+        JFrame frame = new JFrame("My first button");
+        JPanel panel = new JPanel();
 
-        frame.getContentPane().add(button);
+        button = new JButton("Interview");
+        button2 = new JButton("Click me");
+        button.addActionListener(this);
+        button2.addActionListener(this);
+
+        //frame.getContentPane().add(button);
+        panel.add(button);
+        panel.add(button2);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
         frame.setSize(300,300);
         frame.setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent event){
+    public void actionPerformed(ActionEvent event) {
         button.setText("Congratulations! You got hired!");
+        button2.setText("End");
     }
 }
