3e41f6508eff43433fba1ad0cbf727ff5e107f3a
Pavel P <markee109582@yandex.ru>
2024-01-21 17:01:40 +0300

    fix MiniMusicPlayer2

--- LearnWithBook/chapter12/MiniMusicPlayer2.java
+++ LearnWithBook/chapter12/MiniMusicPlayer2.java
@@ -2,7 +2,7 @@ package LearnWithBook.chapter12;
 
 import javax.sound.midi.*;
 
-public class MiniMusicPlayer2 {
+public class MiniMusicPlayer2 implements ControllerEventListener {
 
     public static void main(String[] args) {
         MiniMusicPlayer2 miniplayer = new MiniMusicPlayer2();
@@ -15,11 +15,15 @@ public class MiniMusicPlayer2 {
             Sequencer sequencer = MidiSystem.getSequencer();
             sequencer.open();
 
+            int[] eventsIWant = {127};
+            sequencer.addControllerEventListener(this, eventsIWant);
+
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
 
             for (int i = 5; i < 61; i += 4){
                 track.add(makeEvent(144, 1, i, 100, i));
+                track.add(makeEvent(176, 1, 127, 0, i));
                 track.add(makeEvent(128, 1, i, 100, i + 2));
             }
 
@@ -30,6 +34,11 @@ public class MiniMusicPlayer2 {
         } catch(Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    @Override
+    public void controlChange(ShortMessage event) {
+        System.out.print("la ");
     }
 
     public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
