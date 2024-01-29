e66012b31f24fae0cbae6416165b30da95a80236
Pavel P <markee109582@yandex.ru>
2024-01-29 13:57:04 +0300

    add testBox

---
+++ LearnWithBook/chapter13/testBox.java
@@ -0,0 +1,15 @@
+package LearnWithBook.chapter13;
+
+import javax.swing.*;
+
+public class testBox {
+    public static void main(String[] args) {
+        testBox gui = new testBox();
+        gui.go();
+    }
+    public void go() {
+
+        JFrame frame = new JFrame("Box");
+
+    }
+}
