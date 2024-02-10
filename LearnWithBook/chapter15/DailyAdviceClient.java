81029e9941f54b15931cc4875560ca740045f0fb
Pavel P <markee109582@yandex.ru>
2024-02-10 15:10:57 +0300

    add new chapter 15 + DailyAdviceClient

---
+++ LearnWithBook/chapter15/DailyAdviceClient.java
@@ -0,0 +1,12 @@
+package LearnWithBook.chapter15;
+
+public class DailyAdviceClient {
+    public static void main(String[] args) {
+        DailyAdviceClient client = new DailyAdviceClient();
+        client.go();
+    }
+
+    public void go() {
+
+    }
+}
