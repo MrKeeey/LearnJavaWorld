125da00d1b70f536c4872031d96299c7a2b27f05
Pavel P <markee109582@yandex.ru>
2024-02-10 17:24:53 +0300

    fix Client Server

---
+++ LearnWithBook/chapter15/Client.java
@@ -0,0 +1,33 @@
+package LearnWithBook.chapter15;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.go();
+    }
+
+    public void go() {
+
+        try {
+
+            Socket socket = new Socket("127.0.0.1", 5000);
+            InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+
+            //System.out.println(reader.readLine());
+            //System.out.println(reader.readLine());
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            reader.close();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
---
+++ LearnWithBook/chapter15/Server.java
@@ -0,0 +1,34 @@
+package LearnWithBook.chapter15;
+
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.go();
+    }
+
+    public void go() {
+
+        try {
+            ServerSocket server = new ServerSocket(5000);
+            System.out.println("Started");
+
+            Socket chatSocket = server.accept();
+            System.out.println("Connected");
+
+            PrintWriter writer = new PrintWriter(chatSocket.getOutputStream());
+            writer.println("Hello!");
+            writer.print("I'm Meow ><");
+
+            System.out.println("End work");
+
+            writer.close();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
