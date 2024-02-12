061bf48e7c344e423636ef032f5baad63e1b40a6
Pavel P <markee109582@yandex.ru>
2024-02-12 13:55:00 +0300

    fix count files

--- HelloWorld/CountFiles.java
+++ HelloWorld/CountFiles.java
@@ -5,12 +5,24 @@ import java.io.File;
 public class CountFiles {
     public static void main(String[] args) {
         try {
-            File file[] =  new File("Z:\\ICloud\\Drive\\ILCE-7RM3\\Minsk").listFiles();
-            int count = file.length;
-            System.out.println("Total no. of files : " + count);
+            CountFiles countFiles = new CountFiles();
+            countFiles.numberFilesFolders(new File("Z:\\ICloud\\Drive\\ILCE-7RM3\\Minsk"));
         }
         catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public void numberFilesFolders(File folder) {
+
+        File[] foldersEntry = folder.listFiles();
+        System.out.println(folder + " ---" + foldersEntry.length);
+
+        for (File entry: foldersEntry) {
+            if (entry.isDirectory()) {
+                numberFilesFolders(entry);
+            }
+        }
+
     }
 }
