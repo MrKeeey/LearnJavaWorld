b7e746ac18651cb064c41b15b8b168f24c4941fb
Pavel P <markee109582@yandex.ru>
2024-02-17 13:05:27 +0300

    add TestSync

---
+++ LearnWithBook/chapter15/TestSync.java
@@ -0,0 +1,26 @@
+package LearnWithBook.chapter15;
+
+public class TestSync implements Runnable {
+    private int balance;
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            increment();
+            System.out.println(" Balance: " + balance);
+        }
+    }
+
+    public synchronized void increment() {
+        int i = balance;
+        balance = i + 1;
+    }
+}
+
+class TestSyncTest {
+    public static void main(String[] args) {
+        TestSync job = new TestSync();
+        Thread a = new Thread(job);
+        Thread b = new Thread(job);
+        a.start();
+        b.start();
+    }
+}
