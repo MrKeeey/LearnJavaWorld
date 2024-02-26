9b65a7c179266f871642546dad524f82102ed5f4
Pavel P <markee109582@yandex.ru>
2024-02-26 14:31:09 +0300

    fix rim MyRemote

---
+++ LearnWithBook/chapter18/MyRemoteClient.java
@@ -0,0 +1,21 @@
+package LearnWithBook.chapter18;
+
+import java.rmi.Naming;
+
+public class MyRemoteClient {
+    public static void main(String[] args) {
+        new MyRemoteClient().go();
+    }
+
+    public void go() {
+
+        try {
+            MyRemote service = (MyRemote) Naming.lookup("rmi://127.0.0.1/Remote hello");
+            String line = service.sayHello();
+            System.out.println(line);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+    }
+}
