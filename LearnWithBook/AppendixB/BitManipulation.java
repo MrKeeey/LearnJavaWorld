9ece6dce63c9aa8669a3b7496fc7dca40cf15299
Pavel P <markee109582@yandex.ru>
2024-03-02 17:43:36 +0300

    add BitManipulation

---
+++ LearnWithBook/AppendixB/BitManipulation.java
@@ -0,0 +1,22 @@
+package LearnWithBook.AppendixB;
+
+public class BitManipulation {
+    public static void main(String[] args) {
+        int x = 4, y = 5;
+
+        int z = x & y;
+        System.out.println(z);
+        z = x | y;
+        System.out.println(z);
+        z = x ^ y;
+        System.out.println(z);
+        z = ~x;
+        System.out.println(z);
+        x = x << 2;
+        System.out.println(x);
+        x = x >> 2;
+        System.out.println(x);
+        x = x >>> 2;
+        System.out.println(x);
+    }
+}
