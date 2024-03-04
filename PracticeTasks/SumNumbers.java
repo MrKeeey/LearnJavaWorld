1a6d1c4a05f5c3060efa1cf530501a431e1edbe6
Pavel P <markee109582@yandex.ru>
2024-03-04 22:43:55 +0300

    add task

---
+++ PracticeTasks/SumNumbers.java
@@ -0,0 +1,48 @@
+package PracticeTasks;
+
+import javax.sound.midi.Soundbank;
+import java.net.Socket;
+
+public class SumNumbers {
+    public static final int MAX_LIMIT = 1000;
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < MAX_LIMIT; i++) {
+            int sum = 0;
+            if (i % 3 == 0 && i % 5 != 0) {
+                if (i < 10) {
+                    System.out.print(i + " ");
+                } else {
+                    //new SumNumbers().sum(i);
+                    int buff = i;
+
+                    while (buff != 0) {
+                        sum += (buff % 10);
+                        buff /= 10;
+                    }
+
+                    if (sum < 10) {
+                        System.out.print(i + " ");
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void sum(int number) {
+
+        String buffer = "" + number;
+        int result = 0;
+
+        for (int i = 0; i < buffer.length(); i++) {
+            result += Integer.parseInt(String.valueOf(buffer.charAt(i)));
+        }
+
+        if (result < 10) {
+            System.out.print(buffer + " ");
+        }
+
+    }
+}
