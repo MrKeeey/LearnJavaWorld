6b1355fcd550d44b571b6603d7147b9f30d19b89
Pavel P <markee109582@yandex.ru>
2024-03-05 14:51:23 +0300

    fix HelloWorld

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -6,33 +6,37 @@ public class HelloWorld {
         String HW = "Hello World";
         System.out.println("Hello World!");
         System.out.print("Hello World!".charAt(1) + " ");
-        System.out.println("Hello World!".substring(10,11));
+        System.out.println("Hello World!".substring(10, 11));
         System.out.print(HW.charAt(1) + " ");
-        System.out.println(HW.substring(10,11));
+        System.out.println(HW.substring(10, 11));
 
         //Goodbye world backwards chartAt
         String GW = "Goodbye World!";
-        for (int i = GW.length(); i>0; i--){
-            System.out.print(GW.charAt(i-1));
+        for (int i = GW.length(); i > 0; i--){
+            System.out.print(GW.charAt(i - 1));
         }
         System.out.println();
+        StringBuilder str = new StringBuilder(GW);
+        System.out.println(str.reverse());
 
         //HelloWorld backwards substring
-        for (int i = "Hello World!".length(); i>0; i--){
-            System.out.print("Hello World!".substring(i-1,i));
+        for (int i = "Hello World!".length(); i > 0; i--){
+            System.out.print("Hello World!".substring(i - 1, i));
         }
         System.out.println();
+        str = new StringBuilder("Hello World!");
+        System.out.println(str.reverse());
 
-        //HelloWorld + array
+        //HelloWorld + array + backwards
         char[] arrHW = "Hello World!".toCharArray();
-        for (int c = arrHW.length; c>0; c--){
-            System.out.print(arrHW[c-1]);
+        for (int i = arrHW.length; i > 0; i--){
+            System.out.print(arrHW[i - 1]);
         }
         System.out.println();
 
         //foreach
-        for (char j : arrHW){
-            System.out.print(j);
+        for (char i : arrHW){
+            System.out.print(i);
         }
         System.out.println();
 
