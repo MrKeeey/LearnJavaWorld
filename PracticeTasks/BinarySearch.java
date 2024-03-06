102fe607af28d6d9b72bd988c43ade224f97ca1d
Pavel P <markee109582@yandex.ru>
2024-03-06 12:33:23 +0300

    fix BinarySearch

--- PracticeTasks/BinarySearch.java
+++ PracticeTasks/BinarySearch.java
@@ -1,38 +1,59 @@
 package PracticeTasks;
 
+import java.util.Scanner;
+
 public class BinarySearch {
+
+    int enterGuess = 0;
+
     public static void main(String[] args) {
 
         int[] data = new int[100];
+        int low = 0;
+        int high = data.length - 1;
+        int count = 0;
 
         for (int i = 0; i < data.length; i++) {
             data[i] = i + 1;
         }
 
-        int guess = 55;
-        int low = data[0];
-        int high = data[data.length - 1];
-        int count = 0;
+        System.out.print("Enter your number: ");
+        int guess = new BinarySearch().enter();
 
         while (low <= high) {
 
+            count++;
             int mid = (low + high) / 2;
 
-            if (mid == guess) {
-                System.out.println("Find your guess: " + mid + ", in " + count + " attempts.");
+            if (guess == data[mid]) {
+                System.out.println("Find your guess: " + data[mid] + ", in " + count + " attempts.");
                 break;
             }
 
-            if (mid < guess) {
-                low = mid;
+            if (data[mid] < guess) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-
-            if (mid > guess) {
-                high = mid;
-            }
-
-            count++;
 
         }
+    }
+
+    public int enter () {
+
+        Scanner console = new Scanner(System.in);
+
+        if (!console.hasNextInt()) {
+            System.out.print("Wrong enter. Enter a number from 0 to 100: " );
+            console.next();
+            enter();
+        } else {
+            enterGuess = console.nextInt();
+            if (enterGuess > 100 || enterGuess < 0) {
+                System.out.print("Wrong enter. Enter a number from 0 to 100: " );
+                enter();
+            }
+        }
+        return enterGuess;
     }
 }
