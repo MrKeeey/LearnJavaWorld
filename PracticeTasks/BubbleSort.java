d5e7bd0a148266a18874dc772a535c362d45e163
Pavel P <markee109582@yandex.ru>
2024-03-07 14:16:47 +0300

    fix BubbleSort

--- PracticeTasks/BubbleSort.java
+++ PracticeTasks/BubbleSort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] array = {-5, 6, 234, -23, 4, 6, -8, 0, -1};
+        System.out.println(Arrays.toString(array));
 
         for (int i = array.length - 1; i >= 1 ; i--) {
             for (int j = 0; j < i; j++) {
@@ -12,6 +13,20 @@ public class BubbleSort {
                     int buffer = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = buffer;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] < array[i + 1]) {
+                    int buffer = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = buffer;
+                    flag = true;
                 }
             }
         }
