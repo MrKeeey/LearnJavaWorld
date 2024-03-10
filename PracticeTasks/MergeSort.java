9dd22087aeae5a1b04c84a14e914f8a95fcfe007
Pavel P <markee109582@yandex.ru>
2024-03-10 13:13:22 +0300

    fix MergeSort

--- PracticeTasks/MergeSort.java
+++ PracticeTasks/MergeSort.java
@@ -6,28 +6,49 @@ public class MergeSort {
     public static void main(String[] args) {
         int[] array = {-5, 6, 234, -23, 4, 6, -8, 0, -1};
         System.out.println(Arrays.toString(array));
-        sort(array);
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(sort(array)));
     }
 
     public static int[] sort(int[] array) {
         if (array.length < 2) {
             return array;
         } else {
-            int[] left = Arrays.copyOfRange(array, 0, array.length / 2);
-            int[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
-            System.out.print(Arrays.toString(left));
-            System.out.println(" " + Arrays.toString(right) + "\n");
-            sort(left);
-            sort(right);
-            int[] result = merge(left, right);
-            return result;
+            int[] left = sort(Arrays.copyOfRange(array, 0, array.length / 2));
+            int[] right = sort(Arrays.copyOfRange(array, array.length / 2, array.length));
+            return merge(left, right);
         }
     }
 
-    public static int[] merge(int[] left, int right[]) {
+    public static int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
 
-        int[] result = new int[]{0};
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                result[k] = left[i];
+                i++;
+            } else {
+                result[k] = right[j];
+                j++;
+            }
+            k++;
+            if (i == left.length) {
+                while (j < right.length) {
+                    result[k] = right[j];
+                    k++;
+                    j++;
+                }
+            }
+            if (j == right.length) {
+                while (i < left.length) {
+                    result[k] = left[i];
+                    k++;
+                    i++;
+                }
+            }
+        }
         return result;
     }
 }
