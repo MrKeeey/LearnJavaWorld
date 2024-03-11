52025802dfda3abbe70132b272e619a9a5113a85
Pavel P <markee109582@yandex.ru>
2024-03-11 16:47:10 +0300

    fix sum

--- PracticeTasks/SumNumbers.java
+++ PracticeTasks/SumNumbers.java
@@ -7,15 +7,12 @@ public class SumNumbers {
     public static final int MAX_LIMIT = 1000;
 
     public static void main(String[] args) {
-
         for (int i = 0; i < MAX_LIMIT; i++) {
-
             if (i % 3 == 0 && i % 5 != 0) {
                 if (i < 10) {
                     System.out.print(i + " ");
                 } else {
                     //new SumNumbers().sum(i);
-
                     int buff = i;
                     int sum = 0;
 
@@ -31,10 +28,11 @@ public class SumNumbers {
             }
         }
 
+        int[] array = {-5, 6, 234, -23, 4, -6, -8, 0, -1};
+        System.out.println("\n" + sum(array, array.length - 1));
     }
 
     private void sum(int number) {
-
         String buffer = "" + number;
         int result = 0;
 
@@ -45,6 +43,12 @@ public class SumNumbers {
         if (result < 10) {
             System.out.print(number + " ");
         }
+    }
 
+    private static int sum (int[] array, int length) {
+        if (length < 0) return 0;
+        int result = array[length];
+        result += sum(array, length - 1);
+        return result;
     }
 }
