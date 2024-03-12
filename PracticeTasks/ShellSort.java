a08929f8bd23d11daaa4d0a83234259dbcb16db0
Pavel P <markee109582@yandex.ru>
2024-03-12 12:27:22 +0300

    fix ShellSort

--- PracticeTasks/ShellSort.java
+++ PracticeTasks/ShellSort.java
@@ -8,6 +8,8 @@ public class ShellSort {
         System.out.println(Arrays.toString(array));
         shellSort(array);
         System.out.println(Arrays.toString(array));
+        shellSort2(array);
+        System.out.println(Arrays.toString(array));
     }
 
     public static void shellSort(int[] array) {
@@ -19,6 +21,26 @@ public class ShellSort {
                         int tmp = array[j];
                         array[j] = array[j + gap];
                         array[j + gap] = tmp;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void shellSort2(int[] array) {
+        int h = 1;
+        while (h * 3 < array.length) {
+            h = h * 3 + 1;
+        }
+        for (; h > 0; h /= 3) {
+            for (int i = h; i < array.length; i++) {
+                for (int j = i; j >= h; j -= h) {
+                    if (array[j] > array[j - h]) {
+                        int tmp = array[j];
+                        array[j] = array[j - h];
+                        array[j - h] = tmp;
+                    } else {
+                        break;
                     }
                 }
             }
