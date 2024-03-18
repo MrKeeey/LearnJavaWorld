8fc1ec7c2cd550e723f7b8aa0b5817bec72da7f2
Pavel P <markee109582@yandex.ru>
2024-03-18 15:27:55 +0300

    add leetcode

---
+++ PracticeTasks/LeetCodeMaxLengthStringArray.java
@@ -0,0 +1,35 @@
+package PracticeTasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCodeMaxLengthStringArray {
+    public static void main(String[] args) {
+        List<String> inputArray= new ArrayList<String>();
+        inputArray.add("cha");
+        inputArray.add("r");
+        inputArray.add("act");
+        inputArray.add("ers");
+        maxLength(inputArray);
+    }
+
+    private static int maxLength (List<String> arr) {
+        if (arr.size() == 1) {
+            return arr.get(0).length();
+        }
+        int maxIndex = 0;
+        int indexForRemove = 0;
+        for (int i = 0; i < 2; i++) {
+            int max = arr.get(0).length();
+            for (int j = 1; j < arr.size(); j++) {
+                if (arr.get(j).length() > max) {
+                    max = arr.get(j).length();
+                    indexForRemove = j;
+                }
+            }
+            arr.remove(indexForRemove);
+            maxIndex += max;
+        }
+        return maxIndex;
+    }
+}
---
+++ PracticeTasks/PalindromeInSubstring.java
@@ -0,0 +1,33 @@
+package PracticeTasks;
+
+import javax.sound.midi.Soundbank;
+
+public class PalindromeInSubstring {
+    public static void main(String[] args) {
+        String s = "kipolpikaba";
+        System.out.println(longestPalindrome(s));
+    }
+
+    public static String longestPalindrome(String s) {
+        for (int i = s.length(); i > 0 ; i--) {
+            for (int j = 0; j <= s.length() - i ; j++) {
+                if (isPalindrome(j, j + i, s)) {
+                    return s.substring(j, j + i);
+                }
+            }
+        }
+        return "";
+    }
+
+    private static boolean isPalindrome(int left, int right, String str) {
+        System.out.println(str.substring(left, right));
+        if (left < right - 1) {
+            for (int i = left; i < (right - 1); i++, right--) {
+                if (str.charAt(i) != str.charAt(right - 1)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
