8521ca101cabb698aae3f119988433071baba767
Pavel P <markee109582@yandex.ru>
2024-03-20 13:40:18 +0300

    add leetcode

---
+++ PracticeTasks/LeetCodeReverseInteger.java
@@ -0,0 +1,20 @@
+package PracticeTasks;
+
+public class LeetCodeReverseInteger {
+    public static void main(String[] args) {
+        System.out.println(reverse(1534236469));
+    }
+
+    public static int reverse(int x) {
+        long buff = 0;
+        while (x != 0) {
+            buff *= 10;
+            buff += x % 10;
+            x /= 10;
+            if (buff > Integer.MAX_VALUE || buff < Integer.MIN_VALUE) {
+                return 0;
+            }
+        }
+        return (int) buff;
+    }
+}
