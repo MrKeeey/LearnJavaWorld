7e95942aab7b10f172066a668427691497e27f4c
Pavel P <markee109582@yandex.ru>
2024-03-25 18:16:46 +0300

    fix + add leetcode

---
+++ PracticeTasks/LeetCodeLongestSubstring.java
@@ -0,0 +1,60 @@
+package PracticeTasks;
+
+
+public class LeetCodeLongestSubstring {
+    public static void main(String[] args) {
+        String s = "aabaab!bb";
+        System.out.println(lengthOfLongestSubstring2(s));
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring2(String s) {
+        if (s.length() == 0) return 0;
+        if (s.length() == 1) return 1;
+        StringBuilder result = new StringBuilder();
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (result.toString().contains(String.valueOf(s.charAt(i)))) {
+                if (max < result.length()) {
+                    max = result.length();
+                }
+                while (result.toString().contains(String.valueOf(s.charAt(i)))) {
+                    result.deleteCharAt(0);
+                }
+                result.append(s.charAt(i));
+            } else {
+                result.append(s.charAt(i));
+                if (max < result.length()) {
+                    max = result.length();
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) return 0;
+        if (s.length() == 1) return 1;
+        String result = "";
+        int max = 0;
+        for (int j = 0; j < s.length(); j++) {
+            for (int i = j; i < s.length(); i++) {
+                if (result.contains(String.valueOf(s.charAt(i)))) {
+                    if (max < result.length()) {
+                        max = result.length();
+                    }
+                    result = String.valueOf(s.charAt(i));
+                } else {
+                    result += s.charAt(i);
+                    if (i == s.length() - 1) {
+                        if (max < result.length()) {
+                            max = result.length();
+                        }
+                    }
+                }
+            }
+            result = "";
+        }
+        return max;
+    }
+}
