24626b532f035f95f23be7f5f1dd9b2d2996fa85
Pavel P <markee109582@yandex.ru>
2024-03-27 13:38:16 +0300

    add leetcode

---
+++ PracticeTasks/LC_NumSubarrayProductLessThanK.java
@@ -0,0 +1,46 @@
+package PracticeTasks;
+
+public class LC_NumSubarrayProductLessThanK {
+    public static void main(String[] args) {
+        int[] array = {10, 5, 1, 1};
+        int k = 100;
+        System.out.println(numSubarrayProductLessThanK(array, 100));
+        System.out.println(numSubarrayProductLessThanK2(array, 100));
+    }
+
+    public static int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k == 0) return 0;
+        int count = 0;
+        int product = 1;
+        for (int i = 0; i < nums.length; i++) {
+            int j = i;
+            while (j < nums.length) {
+                product *= nums[j];
+                if (product < k) {
+                    count++;
+                    j++;
+                } else {
+                    break;
+                }
+            }
+            product = 1;
+        }
+        return count;
+    }
+
+    public static int numSubarrayProductLessThanK2(int[] nums, int k) {
+        int count = 0;
+        int product = 1;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            product *= nums[i];
+            while (product >= k && j <= i) {
+                product /= nums[j];
+                j++;
+            }
+            count += i - j + 1;
+            System.out.println(count);
+        }
+        return count;
+    }
+}
