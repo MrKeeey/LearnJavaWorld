413a3957d82c5356c05a4388fcc03b02877d843b
Pavel P <markee109582@yandex.ru>
2024-03-29 15:10:14 +0300

    fix leetcode

---
+++ PracticeTasks/LC_KidsWithCandies.java
@@ -0,0 +1,30 @@
+package PracticeTasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC_KidsWithCandies {
+    public static void main(String[] args) {
+        int[] candies = {4, 2, 1, 1, 2};
+        int myCandies = 1;
+        System.out.println(kidsWithCandies(candies, myCandies));
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        int max = 0;
+        for (int num : candies) {
+            if (max < num) {
+                max = num;
+            }
+        }
+        for (int i : candies) {
+            if (i + extraCandies >= max) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
