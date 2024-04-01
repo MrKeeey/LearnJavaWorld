dacd93183c1fd3bb69140a298d17f4e9a94efbd7
Pavel P <markee109582@yandex.ru>
2024-04-01 12:23:58 +0300

    add leetcode

---
+++ PracticeTasks/LC_MaxArea.java
@@ -0,0 +1,25 @@
+package PracticeTasks;
+
+public class LC_MaxArea {
+    public static void main(String[] args) {
+        int[] water = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(water));
+    }
+
+    public static int maxArea(int[] height) {
+        int resultMaxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int currentMin = Math.min(height[left], height[right]);
+            resultMaxArea = Math.max(currentMin * (right - left), resultMaxArea);
+            while (left < right && height[left] <= currentMin) {
+                left++;
+            }
+            while (left < right && height[right] <= currentMin){
+                right--;
+            }
+        }
+        return resultMaxArea;
+    }
+}
