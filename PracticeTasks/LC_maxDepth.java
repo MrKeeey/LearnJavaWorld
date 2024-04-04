0afc1374ecee5bbcebec366722007041953ad4a3
Pavel P <markee109582@yandex.ru>
2024-04-04 13:18:09 +0300

    add leetcode

---
+++ PracticeTasks/LC_maxDepth.java
@@ -0,0 +1,25 @@
+package PracticeTasks;
+
+public class LC_maxDepth {
+    public static void main(String[] args) {
+        String s = "(1+(2*3)+((8)/4))+1";
+        System.out.println(maxDepth(s));
+    }
+
+    public static int maxDepth(String s) {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+                if (max < count) {
+                    max = count;
+                }
+            }
+            if (s.charAt(i) == ')'){
+                count--;
+            }
+        }
+        return max;
+    }
+}
