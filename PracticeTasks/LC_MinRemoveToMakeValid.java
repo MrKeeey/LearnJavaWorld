3c9af1b07f1d92243e1fb6ede04d864535865da8
Pavel P <markee109582@yandex.ru>
2024-04-06 13:45:37 +0300

    add leetcode

---
+++ PracticeTasks/LC_MinRemoveToMakeValid.java
@@ -0,0 +1,39 @@
+package PracticeTasks;
+
+public class LC_MinRemoveToMakeValid {
+    public static void main(String[] args) {
+        String s = "a)b(c)d";
+        System.out.println(minRemoveToMakeValid(s));
+    }
+
+    public static String minRemoveToMakeValid(String s) {
+        if (s.length() == 0) return "";
+        int count = 0;
+        int pos = 0;
+        StringBuilder str = new StringBuilder(s);
+        while (count <= str.length() - 1) {
+            if (str.charAt(count) == '(') {
+                pos++;
+                count++;
+            } else if (str.charAt(count) == ')') {
+                if (pos == 0) {
+                    str.delete(count, count + 1);
+                } else {
+                    pos--;
+                    count++;
+                }
+            } else {
+                count++;
+            }
+        }
+        count = str.length() - 1;
+        while (count >= 0) {
+            if (pos > 0 && str.charAt(count) == '(') {
+                str.delete(count, count + 1);
+                pos--;
+            }
+            count--;
+        }
+        return str.toString();
+    }
+}
