4ef7e508cf26a98143baa1f19fb8c0b3e829449f
Pavel P <markee109582@yandex.ru>
2024-04-09 13:22:52 +0300

    add leetcode

---
+++ PracticeTasks/LC_TimeRequiredToBuy.java
@@ -0,0 +1,22 @@
+package PracticeTasks;
+
+public class LC_TimeRequiredToBuy {
+    public static void main(String[] args) {
+        int[] tickets = {84, 49, 5, 24, 70, 77, 87, 8};
+        System.out.println(new LC_TimeRequiredToBuy().timeRequiredToBuy(tickets, 3));
+    }
+
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int result = 0;
+        while (tickets[k] > 0) {
+            for (int i = 0; i < tickets.length; i++) {
+                if (tickets[i] > 0) {
+                    tickets[i]--;
+                    result++;
+                }
+                if (tickets[k] == 0) return result;
+            }
+        }
+        return result;
+    }
+}
