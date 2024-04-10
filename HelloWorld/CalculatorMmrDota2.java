fefbe96e7902423e440eae7a66849999e6b83b58
Pavel P <markee109582@yandex.ru>
2024-04-10 21:25:47 +0300

    add leetcode

--- HelloWorld/CalculatorMmrDota2.java
+++ HelloWorld/CalculatorMmrDota2.java
@@ -2,8 +2,8 @@ package HelloWorld;
 
 public class CalculatorMmrDota2 {
     public static void main(String[] args) {
-        int startMmr = 2500;
-        int endMmr = 3500;
+        int startMmr = 2493;
+        int endMmr = 2503;
         SoloBoostMmr soloBoostMmr = new SoloBoostMmr(startMmr, endMmr);
         System.out.println(soloBoostMmr.priceMmrFor100(soloBoostMmr.getStartMmr(), soloBoostMmr.getEndMmr()));
     }
@@ -48,7 +48,7 @@ class SoloBoostMmr {
         double soloPrice5750 = 900;
         double soloPrice6000 = 1100;
 
-        for (int i = start; i < end; i++) {
+        for (int i = start + 1; i <= end; i++) {
             if (i >= 0 && i < 2000) price += soloPrice2000 / 100;
             if (i >= 2000 && i < 2500) price += soloPrice2500 / 100;
             if (i >= 2500 && i < 3000) price += soloPrice3000 / 100;
@@ -61,16 +61,27 @@ class SoloBoostMmr {
             if (i >= 5250 && i < 5500) price += soloPrice5500 / 100;
             if (i >= 5500 && i < 5750) price += soloPrice5750 / 100;
             if (i >= 5750 && i < 6000) price += soloPrice6000 / 100;
-            System.out.println(price);
+            //System.out.println(price);
         }
+
+        double test = 0;
+        for (int i = start; i < end; i += 10) {
+            if (i >= 0 && i < 2000) test += soloPrice2000 / 10;
+            if (i >= 2000 && i < 2500) test += soloPrice2500 / 10;
+            if (i >= 2500 && i < 3000) test += soloPrice3000 / 10;
+            if (i >= 3000 && i < 3500) test += soloPrice3500 / 10;
+            if (i >= 3500 && i < 4000) test += soloPrice4000 / 10;
+            if (i >= 4000 && i < 4500) test += soloPrice4500 / 10;
+            if (i >= 4500 && i < 4750) test += soloPrice4750 / 10;
+            if (i >= 4750 && i < 5000) test += soloPrice5000 / 10;
+            if (i >= 5000 && i < 5250) test += soloPrice5250 / 10;
+            if (i >= 5250 && i < 5500) test += soloPrice5500 / 10;
+            if (i >= 5500 && i < 5750) test += soloPrice5750 / 10;
+            if (i >= 5750 && i < 6000) test += soloPrice6000 / 10;
+            //System.out.println(test);
+        }
+        System.out.println(test);
         return (int) Math.round(price);
-    }
-}
-
-class DuoBoostMMr extends SoloBoostMmr {
-
-    public DuoBoostMMr(int startMmr, int endMmr) {
-        super(startMmr, endMmr);
     }
 }
 
