fdc546846375404563f2e1e30f22d5d96ae3b2fb
Pavel P <markee109582@yandex.ru>
2024-04-18 14:45:38 +0300

    add leetcode

--- PracticeTasks/LC_TreeSmallestFromLeaf.java
+++ PracticeTasks/LC_TreeSmallestFromLeaf.java
@@ -1,6 +1,6 @@
 package PracticeTasks;
 
-public class LC_SmallestFromLeaf {
+public class LC_TreeSmallestFromLeaf {
     public static void main(String[] args) {
         TreeNode tree = new TreeNode(25, new TreeNode(1, new TreeNode(1), new TreeNode(3)), new TreeNode(3, new TreeNode(0), new TreeNode(2)));
         System.out.println(smallestFromLeaf(tree));
