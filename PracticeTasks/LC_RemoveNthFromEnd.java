c8fc9b480b456c769dc0840ff453a71eab035871
Pavel P <markee109582@yandex.ru>
2024-04-23 15:23:32 +0300

    add leetcode

---
+++ PracticeTasks/LC_RemoveNthFromEnd.java
@@ -0,0 +1,28 @@
+package PracticeTasks;
+
+import java.util.Stack;
+
+public class LC_RemoveNthFromEnd {
+    public static void main(String[] args) {
+        ListNode list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        System.out.println(removeNthFromEnd(list, 2));
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.add(head.val);
+            head = head.next;
+        }
+        ListNode result = null;
+        while (!stack.empty()) {
+            n--;
+            if (n != 0) {
+                result = new ListNode(stack.pop(), result);
+            } else {
+                stack.pop();
+            }
+        }
+        return result;
+    }
+}
