f79f6699514143254839f67e178039aa249fe011
Pavel P <markee109582@yandex.ru>
2024-04-24 13:26:45 +0300

    add leetcode

---
+++ PracticeTasks/LC_Tribonacci.java
@@ -0,0 +1,33 @@
+package PracticeTasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LC_Tribonacci {
+    public static void main(String[] args) {
+        System.out.println(tribonacci(5));
+        System.out.println(tribonacci2(5));
+        System.out.println(Integer.MAX_VALUE);
+    }
+
+    public static int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+    }
+
+    public static int tribonacci2(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int[] array = new int[n + 1];
+        array[1] = 1;
+        array[2] = 1;
+
+        for (int i = 3; i < n + 1; i++) {
+            array[i] = array[i - 1] + array[i - 2] + array[i - 3];
+        }
+        return array[n];
+    }
+}
