aea52b4392f3c9a2172307e7978970b09eb17bf4
Pavel P <markee109582@yandex.ru>
2024-05-01 14:59:29 +0300

    add leetcode

---
+++ PracticeTasks/LC_ReversePrefix.java
@@ -0,0 +1,16 @@
+package PracticeTasks;
+
+public class LC_ReversePrefix {
+    public static void main(String[] args) {
+        System.out.println(reversePrefix("abcdefd", 'q'));
+    }
+
+    public static String reversePrefix(String word, char ch) {
+        StringBuilder str = new StringBuilder(word);
+        int count = 0;
+        for (int i = word.indexOf(ch); i >= 0; i--) {
+            str.setCharAt(count++, word.charAt(i));
+        }
+        return str.toString();
+    }
+}
