ab89a2cd19adf8f7f03e0e2a73c4c7613626c77d
Pavel P <markee109582@yandex.ru>
2024-05-10 16:09:24 +0300

    add leetcode

---
+++ PracticeTasks/LC_KthSmallestPrimeFraction.java
@@ -0,0 +1,62 @@
+package PracticeTasks;
+
+import java.util.*;
+
+public class LC_KthSmallestPrimeFraction {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 5};
+        System.out.println(Arrays.toString(kthSmallestPrimeFraction(array, 3)));
+        System.out.println(Arrays.toString(new LC_KthSmallestPrimeFraction().kthSmallestPrimeFraction2(array, 3)));
+    }
+
+    public static int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        int n = arr.length;
+        TreeMap<Double, String> sortedAns = new TreeMap<>();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                sortedAns.put(1.0 * arr[i] / arr[j], arr[i] + "/" + arr[j]);
+            }
+        }
+        for (var elem : sortedAns.entrySet()) {
+            if (k == 1) {
+                String[] result = elem.getValue().split("/");
+                return new int[]{Integer.parseInt(String.valueOf(result[0])), Integer.parseInt(String.valueOf(result[1]))};
+            }
+            k--;
+        }
+        return new int[0];
+    }
+
+    public int[] kthSmallestPrimeFraction2(int[] arr, int k) {
+        int n = arr.length;
+        PriorityQueue<Pair> sortedAnswers = new PriorityQueue<>((o1, o2) -> o1.result > o2.result ? -1 : 1);
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Pair pair = new Pair(arr[i], arr[j]);
+                if (sortedAnswers.size() < k) {
+                    sortedAnswers.offer(pair);
+                } else {
+                    if (sortedAnswers.peek().result > pair.result) {
+                        sortedAnswers.offer(pair);
+                        sortedAnswers.poll();
+                    }
+                }
+            }
+        }
+        Pair pair = sortedAnswers.peek();
+        return new int[]{pair.top, pair.bottom};
+    }
+
+    class Pair {
+        int top;
+        int bottom;
+        double result;
+
+        Pair(int top, int bottom) {
+            this.top = top;
+            this.bottom = bottom;
+            result = 1.0 * top / bottom;
+        }
+    }
+}
