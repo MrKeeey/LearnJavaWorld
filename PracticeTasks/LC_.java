73f62394027c5e6bb9b9cf9f3b63c4d47916c1ba
Pavel P <markee109582@yandex.ru>
2024-05-12 13:20:34 +0300

    add leetcode

---
+++ PracticeTasks/LC_.java
@@ -0,0 +1,32 @@
+package PracticeTasks;
+
+import java.util.Arrays;
+
+public class LC_ {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 2, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}};
+        System.out.println(Arrays.deepToString(largestLocal(matrix)));
+    }
+
+    public static int[][] largestLocal(int[][] grid) {
+        int n = grid.length;
+        int[][] result = new int[n - 2][n - 2];
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                result[i][j] = largestNumInMatrix(grid, i, j);
+            }
+        }
+        return result;
+    }
+
+    public static int largestNumInMatrix(int[][] matrix, int i, int j) {
+        int num = 0;
+        for (int k = i; k < i + 3; k++) {
+            for (int l = j; l < j + 3; l++) {
+                num = Math.max(num, matrix[k][l]);
+            }
+        }
+        return num;
+    }
+}
