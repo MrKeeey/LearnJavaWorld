a1e5e8c1ca20a3f4672e5301da22f409b667640f
Pavel P <markee109582@yandex.ru>
2024-05-19 12:51:46 +0300

    add leetcode

---
+++ PracticeTasks/LC_LengthOfLastWord.java
@@ -0,0 +1,34 @@
+package PracticeTasks;
+
+import java.util.Arrays;
+
+public class LC_LengthOfLastWord {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("   moon  "));
+        System.out.println(lengthOfLastWord2(" meow "));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        int count = 0;
+        int index = s.length() - 1;
+        while (s.charAt(index) == ' '){
+            index--;
+        }
+        for (; index >= 0; index--) {
+            if (s.charAt(index) != ' '){
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+
+    public static int lengthOfLastWord2(String s) {
+        String[] words = s.split(" ");
+        if (words.length == 0) {
+            return 0;
+        }
+        return words[words.length - 1].length();
+    }
+}
