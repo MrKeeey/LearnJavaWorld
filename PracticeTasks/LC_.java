8d9095b208667dcbf1353b41ab26c59154771abc
Pavel P <markee109582@yandex.ru>
2024-05-28 14:55:38 +0300

    add leetcode

---
+++ PracticeTasks/LC_.java
@@ -0,0 +1,31 @@
+package PracticeTasks;
+
+import java.util.Arrays;
+
+public class LC_ {
+    public static void main(String[] args) {
+        System.out.println(equalSubstring("krrgw", "zjxss", 19));
+    }
+
+    public static int equalSubstring(String s, String t, int maxCost) {
+        int n = s.length();
+        int[] costs = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            costs[i] = Math.abs(s.charAt(i) - t.charAt(i));
+        }
+
+        int left = 0;
+        int maxLen = 0;
+        int sum = 0;
+        for (int right = 0; right < n; right++) {
+            sum += costs[right];
+            while (sum > maxCost) {
+                sum -= costs[left];
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+}
