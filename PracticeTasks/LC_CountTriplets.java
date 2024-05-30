3568fc6d1cc88271aff21f4598839b3d80e3ef49
Pavel P <markee109582@yandex.ru>
2024-05-30 14:13:53 +0300

    add leetcode

---
+++ PracticeTasks/LC_CountTriplets.java
@@ -0,0 +1,28 @@
+package PracticeTasks;
+
+public class LC_CountTriplets {
+    public static void main(String[] args) {
+        int[] array = {2, 3, 1, 6, 7};
+        System.out.println(countTriplets(array));
+    }
+
+    public static int countTriplets(int[] arr) {
+        int count = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int a = 0;
+            for (int j = i + 1; j < n; j++) {
+                a ^= arr[j - 1];
+                int b = 0;
+                for (int k = j; k < n; k++) {
+                    b ^= arr[k];
+                    if (a == b) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
