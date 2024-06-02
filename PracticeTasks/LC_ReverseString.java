96bcd5488221577b4b0b5c657d4cb9186391e59b
Pavel P <markee109582@yandex.ru>
2024-06-02 15:35:44 +0300

    add leetcode

---
+++ PracticeTasks/LC_ReverseString.java
@@ -0,0 +1,18 @@
+package PracticeTasks;
+
+public class LC_ReverseString {
+    public static void main(String[] args) {
+        char[] array = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(array);
+    }
+
+    public static void reverseString(char[] s) {
+        int right = s.length - 1;
+        for (int left = 0; left < right; left++, right--) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+        }
+        System.out.println(s);
+    }
+}
