4d71bd7945a3899a29be01794e03b3f359efc491
Pavel P <markee109582@yandex.ru>
2024-06-04 22:51:35 +0300

    add leetcode

--- PracticeTasks/LC_LongestPalindrome.java
+++ PracticeTasks/LC_LongestPalindrome.java
@@ -6,29 +6,17 @@ public class LC_LongestPalindrome {
     }
 
     public static int longestPalindrome(String s) {
-        int count = 0;
         int sum = 0;
         int[] alphabet = new int[58];
         for (int i = 0; i < s.length(); i++) {
             alphabet[s.charAt(i) - 65]++;
         }
         for (int symbols : alphabet) {
-            if (symbols > 1) {
-
-                sum += symbols;
-            }
-            if (symbols == 1) {
-                count++;
+            sum += symbols / 2 * 2;
+            if (sum % 2 == 0 && symbols % 2 == 1) {
+                sum++;
             }
         }
-        if (count == 0) {
-            return sum;
-        } else {
-            if (sum % 2 == 0) {
-                return sum + 1;
-            } else {
-                return sum;
-            }
-        }
+        return sum;
     }
 }
