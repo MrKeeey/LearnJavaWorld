b9aabbf3cce42e54c06464a690570867f0897f33
Pavel P <markee109582@yandex.ru>
2024-06-13 19:25:28 +0300

    add leetcode

---
+++ PracticeTasks/LC_MinMovesToSeat.java
@@ -0,0 +1,21 @@
+package PracticeTasks;
+
+import java.util.Arrays;
+
+public class LC_MinMovesToSeat {
+    public static void main(String[] args) {
+        int[] seats = {2, 2, 6, 6};
+        int[] students = {1, 3, 2, 6};
+        System.out.println(minMovesToSeat(seats, students));
+    }
+
+    public static int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int sum = 0;
+        for (int i = 0; i < seats.length; i++) {
+            sum += Math.abs(seats[i] - students[i]);
+        }
+        return sum;
+    }
+}
