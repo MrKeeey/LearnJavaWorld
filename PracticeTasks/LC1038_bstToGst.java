0ab11662c03aad9b45fd21c241a5abb0a9eaace1
Pavel P <markee109582@yandex.ru>
2024-06-25 14:23:06 +0300

    add LC1038

--- PracticeTasks/LC1038_bstToGst.java
+++ PracticeTasks/LC1038_bstToGst.java
@@ -1,7 +1,7 @@
 package PracticeTasks;
 
 public class LC1038_bstToGst {
-    int max = 0;
+    private int max = 0;
 
     public static void main(String[] args) {
         TreeNode treeNode = new TreeNode(4, new TreeNode(1, new TreeNode(0), new TreeNode(2, null, new TreeNode(3))), new TreeNode(6, new TreeNode(5), new TreeNode(7, null, new TreeNode(8))));
