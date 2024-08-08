53ab1d880cc00c5bd5f2c1a09c572c40f6b3d5f6
Pavel P <markee109582@yandex.ru>
2024-08-08 12:04:24 +0300

    just create

---
+++ PracticeTasks/LC_.java
@@ -0,0 +1,17 @@
+package PracticeTasks;
+
+public class LC_ {
+    public static void main(String[] args) {
+        System.out.println(spiralMatrixIII(1, 4, 0, 0));
+    }
+
+    public static int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int[][] result = new int[rows*cols][2];
+
+        while () {
+
+        }
+
+        return result;
+    }
+}
