4db83796cfbd6c9d4b8769405af0bcd4c47b1006
Pavel P <markee109582@yandex.ru>
2024-08-17 12:41:30 +0300

    add LC885_spiralMatrixIII

--- PracticeTasks/LC885_spiralMatrixIII.java
+++ PracticeTasks/LC885_spiralMatrixIII.java
@@ -1,16 +1,33 @@
 package PracticeTasks;
 
+import java.util.Arrays;
+
 public class LC885_spiralMatrixIII {
     public static void main(String[] args) {
-        System.out.println(spiralMatrixIII(1, 4, 0, 0));
+        System.out.println(Arrays.deepToString(spiralMatrixIII(1, 4, 0, 0)));
     }
 
     public static int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int[][] result = new int[rows*cols][2];
+        int way = 0;
+        int step = 1;
+        int count = 0;
+        int[][] direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int[][] result = new int[rows * cols][2];
 
-//        while () {
-//
-//        }
+        while (count < rows * cols) {
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < step; j++) {
+                    if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols) {
+                        result[count][0] = rStart;
+                        result[count++][1] = cStart;
+                    }
+                    rStart += direction[way][0];
+                    cStart += direction[way][1];
+                }
+                way = (way + 1) % 4;
+            }
+            step++;
+        }
 
         return result;
     }
