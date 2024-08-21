2e61918e8681866c5f6c8409bc8b978f0282a369
Pavel P <markee109582@yandex.ru>
2024-08-21 10:50:44 +0300

    fix LC525_findMaxLength

--- PracticeTasks/LC525_findMaxLength.java
+++ PracticeTasks/LC525_findMaxLength.java
@@ -1,12 +1,35 @@
 package PracticeTasks;
 
-public class LC__525_findMaxLength {
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC525_findMaxLength {
     public static void main(String[] args) {
-        int[] array = {0, 1};
+        int[] array = {1, 1, 0, 0, 1, 1, 0, 1, 1};
         System.out.println(findMaxLength(array));
+        System.out.println(findMaxLength2(array));
     }
 
     public static int findMaxLength(int[] nums) {
+        int maxLength = 0;
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            count += nums[i] == 1 ? 1 : -1;
+            if (map.containsKey(count)) {
+                maxLength = Math.max(maxLength, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+        }
+
+        return maxLength;
+    }
+
+    //TLE
+    public static int findMaxLength2(int[] nums) {
         int maxLength = 0;
         for (int i = 0; i < nums.length; i++) {
             int countZero = 0;
