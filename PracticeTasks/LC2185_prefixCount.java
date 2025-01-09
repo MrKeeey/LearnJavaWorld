8e91c989d6667089910a6ea8e45560446dbb18fc
Pavel P <markee109582@yandex.ru>
2025-01-09 12:44:47 +0300

    add LC2185_prefixCount

---
+++ PracticeTasks/LC2185_prefixCount.java
@@ -0,0 +1,19 @@
+package PracticeTasks;
+
+public class LC2185_prefixCount {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+
+        for (String word : words) {
+            if (word.startsWith(pref)) count++;
+//            if (word.indexOf(pref) == 0) count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"pay", "attention", "practice", "attend"};
+        System.out.println(new LC2185_prefixCount().prefixCount(words, "at"));
+    }
+}
