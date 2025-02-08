414f1b0797ef47c412b5e43a7f7306e106f500e1
Pavel P <markee109582@yandex.ru>
2025-02-08 20:07:34 +0300

    add LC2349_numberContainers

---
+++ PracticeTasks/LC2349_numberContainers.java
@@ -0,0 +1,52 @@
+package PracticeTasks;
+
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class LC2349_numberContainers {
+    public static void main(String[] args) {
+        NumberContainers numberContainers = new NumberContainers();
+        numberContainers.find(10);
+        numberContainers.change(2, 10);
+        numberContainers.change(1, 10);
+        numberContainers.change(3, 10);
+        numberContainers.change(5, 10);
+        numberContainers.find(10);
+        numberContainers.change(1, 20);
+        numberContainers.find(10);
+    }
+}
+
+class NumberContainers {
+
+    private HashMap<Integer, Integer> indexAndNumber;
+    private HashMap<Integer, TreeSet<Integer>> numberAndIndexes;
+
+    public NumberContainers() {
+        numberAndIndexes = new HashMap<>();
+        indexAndNumber = new HashMap<>();
+    }
+
+    public void change(int index, int number) {
+        if (indexAndNumber.containsKey(index)) {
+            int prevNum = indexAndNumber.get(index);
+            numberAndIndexes.get(prevNum).remove(index);
+
+            if (numberAndIndexes.get(prevNum).isEmpty()) {
+                numberAndIndexes.remove(prevNum);
+            }
+        }
+
+        indexAndNumber.put(index, number);
+        numberAndIndexes.putIfAbsent(number, new TreeSet<>());
+        numberAndIndexes.get(number).add(index);
+        //numberAndIndexes.computeIfAbsent(number, x-> new TreeSet<>()).add(index);
+    }
+
+    public int find(int number) {
+        if (numberAndIndexes.containsKey(number)) {
+            return numberAndIndexes.get(number).first();
+        }
+        return -1;
+    }
+}
