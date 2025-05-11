40f611ef6e2f8edf1e7e7b7ee61abb503dc14b5a
Pavel P <markee109582@yandex.ru>
2025-05-11 09:32:02 +0300

    add LC1550_threeConsecutiveOdds

--- PracticeTasks/LC1550_threeConsecutiveOdds.java
+++ PracticeTasks/LC1550_threeConsecutiveOdds.java
@@ -1,21 +1,16 @@
 package PracticeTasks;
 
 public class LC1550_threeConsecutiveOdds {
-    public static void main(String[] args) {
-        int[] array = {1, 1, 1};
-        System.out.println(threeConsecutiveOdds(array));
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1) return true;
+        }
+
+        return false;
     }
 
-    public static boolean threeConsecutiveOdds(int[] arr) {
-        for (int i = 0; i <= arr.length - 3; i++) {
-            if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1) {
-                return true;
-            } else if (arr[i + 1] % 2 == 0) {
-                i = i + 1;
-            } else if (arr[i + 2] % 2 == 0) {
-                i = i + 2;
-            }
-        }
-        return false;
+    public static void main(String[] args) {
+        int[] array = {1, 2, 34, 3, 4, 5, 7, 23, 12};
+        System.out.println(new LC1550_threeConsecutiveOdds().threeConsecutiveOdds(array));
     }
 }
